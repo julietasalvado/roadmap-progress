6596c2831b0ccffd20f77b744d448939e0aa5311
Julieta Salvadó <julieta.salvado@gmail.com>
2022-01-21 23:52:36 -0300

    Refactor Roadmap UI Component

--- src/main/js/components/RoadmapLayout.js
+++ src/main/js/components/RoadmapLayout.js
@@ -1,76 +1,74 @@
-import React, {Component} from "react"
+import React from "react"
 import {Canvas, Node} from "reaflow";
 
-class RoadmapLayout extends Component {
-    render() {
-        const nodes = [
-            {
-                id: '1',
-                height: 100,
-                width: 250,
-                data: {
-                    type: "start",
-                    title: "Java Roadmap"
-                }
-            },
-            {
-                id: '2',
-                height: 125,
-                width: 250,
-                data: {
-                    value: 25,
-                    type: "main-topic",
-                    title: "Frameworks"
-                }
+function RoadmapLayout() {
+    const nodes = [
+        {
+            id: '1',
+            height: 100,
+            width: 250,
+            data: {
+                type: "start",
+                title: "Java Roadmap"
             }
-        ];
-        return <div style={{
-            height: "100vh",
-            overflow: "auto",
-            backgroundColor: "#f5f5f5",
-            backgroundSize: "50px 50px",
-            backgroundImage: "linear-gradient(90deg, #eaeaea 1px, transparent 0), linear-gradient(180deg, #eaeaea 1px, transparent 0)",
-            backgroundPosition: "right -109px bottom -39px",
-            transform: "scale(1)",
-            zIndex: "-10000"
-        }}>
-            <Canvas
-                        direction="DOWN"
-                        nodes={nodes}
-                        edges={[
-                            {
-                                id: '1-2',
-                                from: '1',
-                                to: '2'
+        },
+        {
+            id: '2',
+            height: 125,
+            width: 250,
+            data: {
+                value: 25,
+                type: "main-topic",
+                title: "Frameworks"
+            }
+        }
+    ];
+    return <div style={{
+        height: "100vh",
+        overflow: "auto",
+        backgroundColor: "#f5f5f5",
+        backgroundSize: "50px 50px",
+        backgroundImage: "linear-gradient(90deg, #eaeaea 1px, transparent 0), linear-gradient(180deg, #eaeaea 1px, transparent 0)",
+        backgroundPosition: "right -109px bottom -39px",
+        transform: "scale(1)",
+        zIndex: "-10000"
+    }}>
+        <Canvas
+            direction="DOWN"
+            nodes={nodes}
+            edges={[
+                {
+                    id: '1-2',
+                    from: '1',
+                    to: '2'
+                }
+            ]}
+            node={
+                <Node>
+                    {event => (
+                        <foreignObject height={event.height} width={event.width} x={0} y={0}>
+                            {event.node.data.type === "main-topic" &&
+                            <div style={{padding: 10, textAlign: 'center'}}>
+                                <h3 style={{color: 'white'}}>{event.node.data.title}</h3>
+                                <input type="range" min="1" max="100" value={event.node.data.value}/>
+                            </div>
                             }
-                        ]}
-                        node={
-                            <Node>
-                                {event => (
-                                    <foreignObject height={event.height} width={event.width} x={0} y={0}>
-                                        { event.node.data.type === "main-topic" &&
-                                        <div style={{ padding: 10, textAlign: 'center' }}>
-                                            <h3 style={{ color: 'white' }}>{event.node.data.title}</h3>
-                                            <input type="range" min="1" max="100" value={event.node.data.value} />
-                                        </div>
-                                        }
-                                        { event.node.data.type === "start" &&
-                                        <div style={{
-                                            padding: 10,
-                                            textAlign: 'center',
-                                            borderRadius: "5px 5px 0px 0px"
-                                        }}>
-                                            <h3 style={{ color: 'white' }}>{event.node.data.title}</h3>
-                                        </div>
-                                        }
-                                    </foreignObject>
-                                )}
-                            </Node>
-                        }
-                        onLayoutChange={layout => console.log('Layout', layout)}
-                    />
-        </div>
-    }
+                            {event.node.data.type === "start" &&
+                            <div style={{
+                                padding: 10,
+                                textAlign: 'center',
+                                borderRadius: "5px 5px 0px 0px"
+                            }}>
+                                <h3 style={{color: 'white'}}>{event.node.data.title}</h3>
+                            </div>
+                            }
+                        </foreignObject>
+                    )}
+                </Node>
+            }
+            onLayoutChange={layout => console.log('Layout', layout)}
+        />
+    </div>
 }
 
 export default RoadmapLayout
